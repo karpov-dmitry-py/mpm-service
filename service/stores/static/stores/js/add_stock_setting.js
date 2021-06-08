60e33b48932492ddf522181a89f2ee6ee2d8a1b3
Dmitry Karpov <dem0schase@gmail.com>
2021-06-08 11:26:11 +0300

    updated ui

--- service/stores/static/stores/js/add_stock_setting.js
+++ service/stores/static/stores/js/add_stock_setting.js
@@ -17,12 +17,38 @@ class UI {
 UI.newCondition = function() {
     const conditions = document.getElementById('conditions');
     const types = this.newConditionTypesList();
+    const brands = this.newMultipleSelectList();
     
     const condition = document.createElement('div');
     condition.className = 'condition mb-5';
-
     condition.appendChild(types);
-    conditions.appendChild(condition);
+    condition.appendChild(brands);
+    
+    conditions.appendChild(condition);      
+}
+
+UI.selectOptions = function(list, selected) {
+    for (let i = 0; i < list.options.length; i++) {
+        let option = list.options[i];
+        option.selected = selected;
+    }
+}
+
+UI.selectOptionsByCmd = function(cmd, selected) {
+    const wantedClassName = 'list';
+    parent = cmd.parentElement.parentElement;
+    for (let i = 0; i < parent.children.length; i++) {
+        let child = parent.children[i];
+        
+        // search for class
+        for (let k = 0; k < child.classList.length; k++) {
+            let className = child.classList[k];
+            if (className === wantedClassName) {
+                this.selectOptions(child, selected); 
+                return;
+            }
+        }
+    }
 }
 
 UI.newConditionTypesList = function() {
@@ -46,7 +72,65 @@ UI.newConditionTypesList = function() {
 
     container.appendChild(typesList);
     return container;
+}
 
+UI.getBrands = function() {
+    result = [
+        {id: 1, name: "Brand 1"},
+        {id: 2, name: "Brand 2"},
+        {id: 3, name: "Brand 3"},
+        {id: 4, name: "Brand 4"},
+        {id: 5, name: "Brand 5"},
+        {id: 6, name: "Brand 6"},
+        {id: 7, name: "Brand 7"},
+        {id: 8, name: "Brand 8"},
+        {id: 9, name: "Brand 9"},
+        {id: 10, name: "Brand 10"},
+    ]
+    return result;
+}
+
+UI.newMultipleSelectList = function() {
+    const container = document.createElement('div');
+    container.className = 'form-group brands';
+
+    const toolBar = document.createElement('div'); 
+    toolBar.className = 'multiple-list-tool-bar';
+
+    label = document.createElement('label');
+    label.className = 'toolbar-lbl';
+    label.appendChild(document.createTextNode('Бренд'));
+    toolBar.appendChild(label);
+    
+    selectAll = document.createElement('span');
+    selectAll.className = 'toolbar-cmd text-muted';
+    selectAll.setAttribute('onclick', 'UI.selectOptionsByCmd(this, true);');
+    selectAll.appendChild(document.createTextNode('выделить все'));
+    toolBar.appendChild(selectAll);
+    
+    unselectAll = document.createElement('span');
+    unselectAll.className = 'toolbar-cmd text-muted';
+    unselectAll.setAttribute('onclick', 'UI.selectOptionsByCmd(this, false);');
+    unselectAll.appendChild(document.createTextNode('снять выделение'));
+    toolBar.appendChild(unselectAll);
+    
+    container.appendChild(toolBar);
+    
+    const list = document.createElement('select');
+    list.multiple = true;
+    list.size = 6;
+    list.className = 'csvselect form-control list';
+    
+    const brands = this.getBrands();
+    brands.forEach(function(brand) {
+        const option = document.createElement('option');
+        option.value = brand.id;
+        option.text = brand.name;
+        list.appendChild(option);
+    });
+
+    container.appendChild(list);
+    return container;
 }
 
 UI.getConditionTypes = function() {
